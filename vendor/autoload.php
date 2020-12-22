d80e5899fe4bde55b0276efcbd2de4fb87694d35
doctorengi <43904497+doctorengi@users.noreply.github.com>
2020-12-23 04:57:00 +0700

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,4 +1,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitea0390d6cea695588c7a75c7618ffacb::getLoader();-- 
+return ComposerAutoloaderInitea0390d6cea695588c7a75c7618ffacb::getLoader();
