f53a5b6126239d3481c46f569a59bb19014115eb
doctorengi <43904497+doctorengi@users.noreply.github.com>
2020-12-23 05:13:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,8 +5,7 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = '86uJdolVJGA/6v+CevfONIOVLqi5gYZDak7my2N0ujQnDjurF+bkF03cvru9Gyi3WskeCtnf58Tduf0iuHnMzFArVvWImj5wANabkSZKVOV2X1EwelKGuGkyKKJeTaBF8Ves96fBFsRn3cqbXxeLjwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '3b79370d5e86b13f51b185e1b3adbadb';
-$idPush = 'U0007134473ad3d89728115a6d41d0304'// Get POST body content
+
 
 
 
