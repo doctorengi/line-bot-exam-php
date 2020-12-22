7a5f8eff04bc61f76a12bad4fd39d9b6894a9ec4
doctorengi <43904497+doctorengi@users.noreply.github.com>
2020-12-23 05:12:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -8,12 +8,7 @@ $access_token = '86uJdolVJGA/6v+CevfONIOVLqi5gYZDak7my2N0ujQnDjurF+bkF03cvru9Gyi
 $channelSecret = '3b79370d5e86b13f51b185e1b3adbadb';
 $idPush = 'U0007134473ad3d89728115a6d41d0304'// Get POST body content
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
 
